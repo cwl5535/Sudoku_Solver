4a6b4b2dad6973639e54114f208cf950707fa9fd
Colton Layhue <layhue497@gmail.com>
2022-04-10 20:07:59 -0400

    initial commit

--- classes.py
+++ classes.py
@@ -1,22 +1,15 @@
-with open('sudoku1.csv', newline = '') as file: 
-    for r in file:
-        
-    # puzzle = csv.reader(file)
-    # row = []
-    # for r in puzzle:
-    #     for number in r[0]:
-            
-    #     edit = r[0].replace(";", "0")
-    #     row.append(edit)
-    #     print(row)
-
-
-    #     row = str(r)
-    #     print(row[0])
-        # row = str(r)
-        # for letter in row: 
-        #     print(letter)
-
-# variable = "test"
-# for item in variable: 
-#     print(item)
+with open('sudoku1.csv', newline = '') as file:
+    for line in file.readlines():
+        row = ''
+        for char in line: 
+            if (char == ";"): 
+                row += "0;"
+            else: 
+                row += (char + ";")
+        print(row)
+        # for char in line:
+        #     new_char = char.strip("\r")
+        #     blank_row.append(char)
+        # print(blank_row)
+    #         edited_row = row.append(char).replace(";", "0")
+    # print(edited_row)
