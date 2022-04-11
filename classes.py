aee4ec9dfcb7dd5b54593adba134f3b556b79209
Colton Layhue <layhue497@gmail.com>
2022-04-10 22:57:09 -0400

    testing

--- classes.py
+++ classes.py
@@ -1,12 +1,19 @@
 with open('sudoku1.csv', newline = '') as file:
     for line in file.readlines():
         row = ''
-        for char in line: 
-            if (char == ";"): 
-                row += "0;"
+        for char in line:
+            prev_char = line[(line.index(char)-1)] 
+            if (char == ";" and prev_char == ";"):
+                row += ("0;")
             else: 
                 row += (char + ";")
+
+            # if (line[0] == ";") or (line[-1] == ";"): 
+            #     row += ("0" + char)
+            # elif (char != ';'): 
+            #     row += (char + ";")
         print(row)
+        # print(row)
         # for char in line:
         #     new_char = char.strip("\r")
         #     blank_row.append(char)
