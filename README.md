0858617e9e2075111dc701f11a09e6edf565bb03
Colton Layhue <layhue497@gmail.com>
2022-04-10 20:05:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CS5007 Final Project
---
+++ classes.py
@@ -0,0 +1,22 @@
+with open('sudoku1.csv', newline = '') as file: 
+    for r in file:
+        
+    # puzzle = csv.reader(file)
+    # row = []
+    # for r in puzzle:
+    #     for number in r[0]:
+            
+    #     edit = r[0].replace(";", "0")
+    #     row.append(edit)
+    #     print(row)
+
+
+    #     row = str(r)
+    #     print(row[0])
+        # row = str(r)
+        # for letter in row: 
+        #     print(letter)
+
+# variable = "test"
+# for item in variable: 
+#     print(item)
