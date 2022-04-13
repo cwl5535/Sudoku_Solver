0a41a43871d467edfb06696d3b33b330372cc25d
Colton Layhue <layhue497@gmail.com>
2022-04-12 23:14:48 -0400

    data format with zeros

--- classes.py
+++ classes.py
@@ -1,22 +1,16 @@
 with open('sudoku1.csv', newline = '') as file:
-    for line in file.readlines():
-        row = ''
+    for lines in file.readlines():
+        line = ((lines).rstrip())
+        row = []
+        idx = -1
         for char in line:
-            prev_char = line[(line.index(char)-1)] 
-            if (char == ";" and prev_char == ";"):
-                row += ("0;")
-            else: 
-                row += (char + ";")
-
-            # if (line[0] == ";") or (line[-1] == ";"): 
-            #     row += ("0" + char)
-            # elif (char != ';'): 
-            #     row += (char + ";")
+            row.append(char)
+        for char in row: 
+            if char == row[idx]:
+                row.insert(idx+1, "0")
+            elif row[0] == ";":
+                row.insert(0, "0")
+            elif row[-1] == ";":
+                row.append("0")
+            idx += 1
         print(row)
-        # print(row)
-        # for char in line:
-        #     new_char = char.strip("\r")
-        #     blank_row.append(char)
-        # print(blank_row)
-    #         edited_row = row.append(char).replace(";", "0")
-    # print(edited_row)
